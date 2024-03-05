8906d49b47cecdff7289f8495066cbe187c48602
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-03-05 05:56:59 +0100

    add working binary converter

--- binaryConvert.js
+++ binaryConvert.js
@@ -1,1 +1,15 @@
-test
+function binaryConvert(num){
+  let power = 1;
+  let binary = 0;
+
+  while (num) {
+    const rem = num % 2;
+    num = Math.floor(num / 2);
+    binary = rem * power + binary;
+    power *= 10;
+  }
+
+  return binary;
+}
+
+binaryConvert(5);
