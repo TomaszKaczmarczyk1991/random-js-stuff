5c47deb7a976743fb8412b809bd199419f799c13
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-03-09 15:20:10 +0100

    Update quizzes.js

--- quizzes.js
+++ quizzes.js
@@ -1,9 +1,14 @@
 // 09.03.2024
 // 1) 
-
 for(let i = 1; i < 5; i++){
     if(i === 3) continue;
     console.log(i)
 }
 
 // output: 1, 2, 4
+
+// 2)
+let x = true === []; // boolean, false
+let y = true === ![]; // object, false
+
+console.log(x + y); // false + false = 0
