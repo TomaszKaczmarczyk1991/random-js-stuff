926213d40d3edca060b6cc51fe55bbac492a8aef
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-03-12 19:16:57 +0100

    add stringMethods.js

---
+++ stringMethods.js
@@ -0,0 +1,18 @@
+// JS string methods
+
+// 1) substring()
+let text = "Uszanowanko!";
+let result = text.substring(1);
+console.log(result); // szanowanko!
+
+result = text.substring(3, 6);
+console.log(result); // ano
+
+// 2) substr()
+result = text.substr(3, 6);
+console.log(result); // anowan
+// (the second parameter specifies length)
+
+// 3) chatAt()
+result = text.charAt(0)
+console.log(result); // U
