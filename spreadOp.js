6c7b1bc4af33e7bcc8f293d993b3f1580b0b01dd
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-03-12 16:55:10 +0100

    add spreadOp.js

---
+++ spreadOp.js
@@ -0,0 +1,15 @@
+// Spread Operator Use Cases:
+
+// 1)
+const str = "Thomas";
+const arr = [...str];
+
+console.log(arr); // ["T", "h", "o", "m", "a", "s"]
+
+// 2)
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const mergedArr = [...arr1, ...arr2];
+
+console.log(mergedArr); // [1, 2, 3, 4, 5, 6]
