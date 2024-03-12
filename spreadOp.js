1984b3c86aa5cf96cf428444d6abc4849ca69df1
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-03-12 16:57:57 +0100

    Update spreadOp.js

--- spreadOp.js
+++ spreadOp.js
@@ -7,9 +7,15 @@ const arr = [...str];
 console.log(arr); // ["T", "h", "o", "m", "a", "s"]
 
 // 2)
-
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 const mergedArr = [...arr1, ...arr2];
 
 console.log(mergedArr); // [1, 2, 3, 4, 5, 6]
+
+// 3)
+const originalArr = ['zero', 'one'];
+const copy = [...originalArr];
+
+console.log(originalArr); // ['zero', 'one']
+console.log(copy); // ['zero', 'one'] 
