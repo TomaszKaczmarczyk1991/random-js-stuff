d980f77b02b52daebe06c2a3d503ec7c000efad8
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2024-05-14 17:22:38 +0200

    add enqueue and dequeue methods

--- queue.js
+++ queue.js
@@ -1,5 +1,13 @@
 class Queue {
   constructor(){
     this.queue = [];
-    }
+    }  
+
+  enqueue(elem) {
+      this.queue.push(elem);
+  }
+  
+  dequeue(elem) {
+      return this.queue.shift();
+  }
 }
